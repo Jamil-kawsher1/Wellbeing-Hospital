d9700bab64233fe902a982d85485a271e165ddc8
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2021-10-18 10:38:40 +0600

    Navbar nad basic component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation/Navigation';
 
 function App() {
 
 
   return (
     <div className="App">
-
+      <Navigation></Navigation>
     </div>
   );
 }
---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand className="text-start"><Link to="/" className='linkstye'>Baseline Photography</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link ><Link to='/' className='linkstye'  >Home</Link></Nav.Link>
+                            <Nav.Link ><Link to='/about' className='linkstye'>About</Link></Nav.Link>
+                            <Nav.Link ><Link to="/services" className='linkstye'>Services</Link></Nav.Link>
+                            <Nav.Link > <Link to='/contact' className='linkstye'>Contact US</Link></Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Navigation;
